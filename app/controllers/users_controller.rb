defc5762ec0ca20aca210da2bdb494137c3e661b
koh-suzuki <k.s_coward_0629@icloud.com>
2019-08-08 03:49:51 +0000

    10章10.9.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   def show
     @first_day = Date.current.beginning_of_month
     @last_day = @first_day.end_of_month
+    @worked_sum = @attendances.where.not(started_at: nil).count
   end
 
   def new
